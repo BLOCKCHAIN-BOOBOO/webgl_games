de931f5474a023c3cce3d0e86a9d6643d3aa488f
UiUxDesignerParamesh <naveennani9100@gmail.com>
2023-03-07 11:38:58 +0530

    update

--- src/components/contet.js
+++ src/components/contet.js
@@ -5,27 +5,30 @@ import axios from "axios";
 const Content = ({ id }) => {
   const [gamecomments, setgamecomments] = useState([]);
   const [info, setinfo] = useState([]);
+  const [comment, setcomment] = useState("");
 
-const savecomment=async()=>{
-     try {
-       await axios
-         .get(`https://html-game-api.kryptofam.com/game?id=${id}`)
-         .then((res) => {
-           console.log(res?.data?.data);
-           setinfo(res?.data?.data);
-         });
-     } catch (err) {
-       console.log(err);
-     }
-}
+  const savecomment = async () => {
+    try {
+      console.log(comment);
+      await axios
+        .get(`https://html-game-api.kryptofam.com/game?id=${id}`)
+        .then((res) => {
+          console.log(res?.data?.data);
+          setinfo(res?.data?.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const fetchgamedetails = async () => {
     try {
       await axios
         .get(`https://html-game-api.kryptofam.com/game?id=${id}`)
         .then((res) => {
-          console.log(res?.data?.data);
+          console.log(res?.data?.data?.comments);
           setinfo(res?.data?.data);
+          setgamecomments(res?.data?.data?.comments);
         });
     } catch (err) {
       console.log(err);
@@ -41,11 +44,14 @@ const savecomment=async()=>{
         <span className="border-b-2 border-red-500">More Information</span>
         <i class="fa fa-angle-down text-xl font-semibold self-center align-middle text-red-500"></i>
       </div>
-      {/* <div>{info.description}</div> */}
+      <div>{info.description}</div>
       <div className="flex flex-row self-center text-center py-5 align">
         <input
           type="text"
           name="search"
+          onChange={(e) => {
+            setcomment(e.target.value);
+          }}
           className="header-search bg-red-200 
          b-2 px-3 p-4 h-full dark:focus:border-red-300 focus:ring-red-300 focus:border-red-300 border-0
       border-red-300 placeholder-white font-semibold focus:outline-none 
@@ -57,18 +63,12 @@ const savecomment=async()=>{
           onClick={savecomment}
           className="bg-red-500 w-60 text-white font-semibold -ml-20 px-12 text-xs rounded-full "
         >
-          {" "}
           Send Comment
         </button>
       </div>
-
-      {/* <div className="text-sm font-semibold pt-10">This content is constant no need to change the content for any game</div>
-
-    <div className="flex m-3">
-<button className="bg-blue-400 text-white text-sm py-2 px-4 m-2" onClick={() => video("https://nft.radiqal.com")}>video1</button>
-<button className="bg-blue-600 text-white text-sm py-2 px-4 m-2" onClick={()=>video("http://127.0.0.1:5500/index.html")}>video2</button>
-
-    </div> */}
+      {gamecomments.map((comment) => {
+        return <div key={comment._id}>{comment.text}</div>;
+      })}
     </div>
   );
 };
