eb19bc7db65da15321a14d0adf1776e055e32d1b
Himani Bondalapati <himanidtpv@gmail.com>
2023-03-16 18:11:47 +0530

    design changes

--- src/components/game.js
+++ src/components/game.js
@@ -40,13 +40,13 @@ const Game = () => {
     fetchgamedetails();
   }, []);
   return (
-    <div className="lg:ml-32 md:ml-32 sm:ml-32">
+    <div className="lg:ml-32 xl:py-4 md:py-2 sm:py-2 py-2 md:ml-32 sm:ml-32">
     <div className=" relative md:flex flex-col md:items-left md:justify-between px-10">
               {/* container */}
               <div className="lg:w-full text-left mt-5 sm:mt-5 md:mt-5 xl:mt-0">
                 <div className="flex justify-between flex-row xl:flex-row md:flex-row sm:flex-row ">
                   <div className="flex self-center m-2 align-middle">
-                    <img className="" height="120" width="120" src={bbglogo} />
+                    <img className="" height="150" width="150" src={bbglogo} />
                   </div>
                  
                   <div className="flex xl:w-32 md:w-40 sm:w-32 w-32 flex-row header-search rounded-lg float-right bg-red-200">
